4d9409bf49e9d654d561e2818980afbba8d9af06
vinitakurhade <165883699+vinitakurhade@users.noreply.github.com>
2024-04-19 14:30:55 +0530

    Update LambdaExpression.java

--- LinksAssignment/src/test/java/org/link/LambdaExpression.java
+++ LinksAssignment/src/test/java/org/link/LambdaExpression.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 public class LambdaExpression {
-public static void main(String[] args) throws IOException, InterruptedException
+public static void main(String[] args) 
 	{
 		WebDriver driver = new EdgeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
